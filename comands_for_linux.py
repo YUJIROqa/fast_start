76a25ccc1843cc8b79f8ccc63a4ffb341f44361a
YUJIRO <egor.krasavchenko@gmail.com>
2025-05-22 15:03:04 +0300

    added python file from 3d lesson and commands for linux

---
+++ comands_for_linux.py
@@ -0,0 +1,32 @@
+mkdir name_folder #создать папку
+ls #посмотреть что в папке
+cd name_folder #перейти в папку
+cd .. #выйти из папки
+clear #очистить терминал
+mv old_name_file new_name_file #переименовать файл
+rm name_file #удалить файл
+rm -r name_folder #удалить папку
+cp name_file new_name_file #скопировать файл
+cp -r name_folder new_name_folder #скопировать папку
+pwd #показать текущую дирректорию/путь
+ls -l #показать все файлы и папки в текущей директории
+ls -a #показать все файлы и папки в текущей директории, включая скрытые
+cd  ../ #выйти на уровень вверх
+cd - #вернуться назад
+touch name_file #создать файл
+mkdir name_folder #создать папку
+rm file_name #удалить файл
+rm -r dir_name #удалить папку
+cp file_name dir_name #скопировать файл в папку
+cat name_file #вывести содержимое файла
+less name_file #просмотреть содержимое файла
+head name_file #вывести первые 10 строк файла
+tail name_file #вывести последние 10 строк файла
+grep name_word name_file #найти слово в файле
+sudo #команда для суперпользователя
+apt update #обновить список пакетов
+apt install name_package #установить пакет
+clear #очистить терминал
+history #посмотреть историю команд
+
+
