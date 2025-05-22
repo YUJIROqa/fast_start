943290f531fbb6a5fa22f119d8349a2525e31eca
YUJIRO <egor.krasavchenko@gmail.com>
2025-05-22 08:42:54 +0300

    added 1st lesson

---
+++ 1_lessons/lesson1_python/data.py
@@ -0,0 +1,4 @@
+python --version #windows узнать версию python
+python3 --version #linux узнать версию python
+pip --version #узнать версию pip - пакетный менеджер для установки и управления Python-пакетами.
+]
